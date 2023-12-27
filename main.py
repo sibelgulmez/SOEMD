5f97ef1f6539e78e3c78e73f12b4249fe18a05a4
sibelgulmez <75502894+sibelgulmez@users.noreply.github.com>
2023-12-27 12:20:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from lib import disassembler, graph, w2v, dl
-import csv, random, numpy, os
+import csv, random, numpy, os, time
 import numpy as np
 
 class file:
@@ -13,7 +13,7 @@ class file:
             self.edge_file_path += ".edge"
         self.edge_ratio = edge_ratio
         self.disassembler_object = disassembler.opcode(opcode_file_path)
-    
+
     def calculateFrequencies(self, frequency_dict):
         """
             Updates the frequency_dict with the new file whose opcode file path is given with opcode_file_path.
@@ -31,10 +31,10 @@ class file:
         del(disassembler_object)
         return frequency_dict
 
-    
+
     def generateEdgeDict(self):
         """
-            Reads the edge file and generates edge dictionary. 
+            Reads the edge file and generates edge dictionary.
         """
         edge_dict = dict()
         f = open(self.edge_file_path, "r")
@@ -53,7 +53,7 @@ class file:
                 edge_dict[edge_source][edge_destination] = int(frequency)
         return edge_dict
 
-    
+
     def removeEdges(self, edge_dict):
         """
             Remove some edges according to the ratio.
@@ -103,7 +103,7 @@ class dataset:
             self.readNodes()
 
 
-    
+
     def generateFileObjects(self):
         """
             Generates an edge dictionary for each file and fill them into malware_files and benign_files
@@ -117,9 +117,9 @@ class dataset:
             self.benign_files.append(file(benign_opcodeFile_path, benign_edgeFile_path, self.edge_ratio))
         self.files = self.malware_files + self.benign_files
 
-    
+
     def extractNodes(self):
-        """ 
+        """
             Extracts node_count different nodes for malwares and also node_count different nodes for benigns. These nodes are the most frequently seen nodes and they are used to start a random walk.
         """
         print("Extracting nodes.")
@@ -141,9 +141,9 @@ class dataset:
     def save_nodes(self):
         """
         save the nodes in a file for easy use.
-        :return: 
+        :return:
         """
-        f = open("nodes.txt", "w")
+        f = open("nodes_rw.txt", "w")
         for node in self.malware_nodes:
             f.write(node + "\n")
         for node in self.benign_nodes:
@@ -153,10 +153,10 @@ class dataset:
     def readNodes(self):
         """
         if nodes are already generated and saved, read them.
-        :return: 
+        :return:
         """
         print("Reading nodes.")
-        f = open("nodes.txt", "r")
+        f = open("nodes_rw.txt", "r")
         content = f.read().split("\n")
         f.close()
         self.malware_nodes = content[:self.node_count]
@@ -171,6 +171,7 @@ class soemd:
         :param edge_ratio:  default = 4
         :param sequence_length: length of the random walks, default = 50
         :param sequences_per_node: number of random walks to create per a node. the number of random walks per a sample becomes sequences_per_node*node_count
+        :param train_size: percentage of train data
 
 
         The directory hierarchy:
@@ -180,7 +181,7 @@ class soemd:
         -------opcode     -> this folder (will) contain opcode files
         -------edge       -> this folder (will) contain edge files
     """
-    def __init__(self, malware_dataset_directories = [], benign_dataset_directories = [], create_nodes = True, node_count = 15, edge_ratio = 4, sequence_length = 50, sequences_per_node = 3):
+    def __init__(self, malware_dataset_directories = [], benign_dataset_directories = [], create_nodes = True, node_count = 15, edge_ratio = 4, sequence_length = 50, sequences_per_node = 3, train_size = 0.9):
         self.malware_dataset_directories = malware_dataset_directories
         self.benign_dataset_directories = benign_dataset_directories
         self.create_nodes = create_nodes
@@ -188,16 +189,19 @@ class soemd:
         self.edge_ratio = edge_ratio
         self.sequence_length = sequence_length
         self.sequences_per_node = sequences_per_node
+        self.train_size = train_size
+        self.X_train = []
+        self.y_train = []
+        self.X_test = dict()
+        self.y_test = []
         self.dataset = None # single dataset object, not a list
         self.malware_opcode_file_list = [] # combined opcode file list of malware samples
         self.benign_opcode_file_list = []  # combined opcode file list of benign samples
-        self.malware_randomWalks_dict = dict()
-        self.benign_randomWalks_dict = dict()
         self.generateOpcodeFiles()
         self.generateEdgeFiles()
         self.generateDataset()
 
-    
+
     def generateOpcodeFiles(self):
         """
             Generates opcode files.
@@ -206,7 +210,7 @@ class soemd:
             self.malware_opcode_file_list += disassembler.createOpcodeFiles(malware_dataset_directory)
         for benign_dataset_directory in self.benign_dataset_directories:
             self.benign_opcode_file_list += disassembler.createOpcodeFiles(benign_dataset_directory)
-    
+
     def generateEdgeFiles(self):
         """
             Generates edge files.
@@ -216,15 +220,13 @@ class soemd:
         for benign_dataset_directory in self.benign_dataset_directories:
             graph.generateEdgeFiles(benign_dataset_directory)
 
-    
+
     def generateDataset(self):
         """
             Creates dataset object.
         """
         self.dataset = dataset(self.malware_opcode_file_list, self.benign_opcode_file_list, self.node_count, self.edge_ratio, self.create_nodes)
 
-
-    
     def generateSequence(self, edge_dict, last_used_opcode):
         """
             Generates sequences
@@ -240,36 +242,37 @@ class soemd:
             next_opcode = possible_destinations[randomnumber]
             randomnumbers = []
             randomnumbers.append(randomnumber)
-            while (j != self.sequence_length - 2 and next_opcode not in edge_dict.keys()):
+
+            # eğer seçilen opcode ve son 10 opcode aynı ise farklı bir opcode'a gitsin.
+            flag = True if len(seq) > 10 and list(set((seq[::-1])[:10])) == [next_opcode] else False
+            while (j != self.sequence_length - 2 and next_opcode not in edge_dict.keys() or flag):
                 if (len(randomnumbers) == len(possible_destinations)):
                     return None
                 while (randomnumber not in randomnumbers):
                     randomnumber = random.randint(0, len(possible_destinations) - 1)
                 randomnumbers.append(randomnumber)
                 next_opcode =  possible_destinations[randomnumber]
+                flag = True if len(seq) > 10 and list(set((seq[::-1])[:10])) == [next_opcode] else False
             seq.append(next_opcode)
             last_used_opcode = next_opcode
         return seq
-
-
-    
-    def generateRandomWalks(self):
-        """
-            Generates random walks with the help of generateSequence
-        """
-        print("Generating random walks")
-        # generate random walks for malwares
-        for malware_sample in self.dataset.malware_files:
-            edge_dict = malware_sample.getCleanedEdges()
+    def randomWalkGeneration_helper_train(self, file_list, isMalware):
+        data = []
+        labels = []
+        if isMalware:
+            nodes = self.dataset.malware_nodes
+        else:
+            nodes = self.dataset.benign_nodes
+        for sample in file_list:
+            edge_dict = sample.getCleanedEdges()
             if (len(list(edge_dict.keys()))) == 0:
                 continue
-            self.malware_randomWalks_dict[malware_sample.file_hash] = []
             for starting_node_index in range(self.node_count):
-                if self.dataset.malware_nodes[starting_node_index] in edge_dict.keys():
-                    starting_node = self.dataset.malware_nodes[starting_node_index]
+                if nodes[starting_node_index] in edge_dict.keys():
+                    starting_node = nodes[starting_node_index]
                 else:
                     if len(list(edge_dict.keys())) > starting_node_index:
-                        starting_node = list(edge_dict.keys())[starting_node_index]  # if node was found in the sample (sample is a zero-day malware), then the first opcode of the file is the starting node
+                        starting_node = list(edge_dict.keys())[starting_node_index]  # if node was found in the sample (sample is a zero-day software), then the first opcode of the file is the starting node
                     else:
                         starting_node = list(edge_dict.keys())[0]
                 for i in range(self.sequences_per_node):
@@ -279,81 +282,75 @@ class soemd:
                         seq = self.generateSequence(edge_dict, starting_node)
                         trials += 1
                     if seq != None and len(seq) == self.sequence_length:
-                        self.malware_randomWalks_dict[malware_sample.file_hash].append(seq)
-            if(len(self.malware_randomWalks_dict[malware_sample.file_hash]) != self.sequences_per_node*self.node_count):
-                del(self.malware_randomWalks_dict[malware_sample.file_hash])
-        # generate random walks for malwares
-        for benign_sample in self.dataset.benign_files:
-            edge_dict = benign_sample.getCleanedEdges()
+                        data.append(seq)
+                        labels.append(isMalware)
+        return data, labels
+    def randomWalkGeneration_helper_test(self, file_list, isMalware):
+        data = dict()
+        labels = []
+        nodes = self.dataset.malware_nodes + self.dataset.benign_nodes
+        for sample in file_list:
+            data[sample.file_hash] = []
+            edge_dict = sample.getCleanedEdges()
             if (len(list(edge_dict.keys()))) == 0:
                 continue
-            self.benign_randomWalks_dict[benign_sample.file_hash] = []
             for starting_node_index in range(self.node_count):
-                if self.dataset.benign_nodes[starting_node_index] in edge_dict.keys():
-                    starting_node = self.dataset.benign_nodes[starting_node_index]
+                if nodes[starting_node_index] in edge_dict.keys():
+                    starting_node = nodes[starting_node_index]
                 else:
-                    starting_node = list(edge_dict.keys())[0]  # if node was found in the sample (sample is a zero-day benign), then the first opcode of the file is the starting node
+                    if len(list(edge_dict.keys())) > starting_node_index:
+                        starting_node = list(edge_dict.keys())[starting_node_index]  # if node was found in the sample (sample is a zero-day software), then the first opcode of the file is the starting node
+                    else:
+                        starting_node = list(edge_dict.keys())[0]
                 for i in range(self.sequences_per_node):
                     seq = self.generateSequence(edge_dict, starting_node)
                     trials = 0
-                    while (seq == None and trials < 10):
+                    while(seq == None and trials < 10):
                         seq = self.generateSequence(edge_dict, starting_node)
                         trials += 1
                     if seq != None and len(seq) == self.sequence_length:
-                        self.benign_randomWalks_dict[benign_sample.file_hash].append(seq)
-            if(len(self.benign_randomWalks_dict[benign_sample.file_hash]) != self.sequences_per_node*self.node_count):
-                del(self.benign_randomWalks_dict[benign_sample.file_hash])
+                        data[sample.file_hash].append(seq)
+            labels.append(isMalware)
+        return data, labels
+    def generateRandomWalks(self):
+        """
+            Generates random walks with the help of generateSequence
+        """
+        print("Generating random walks. ", end="")
+        random.shuffle(self.dataset.malware_files)
+        random.shuffle(self.dataset.benign_files)
 
-def train_test_split(malware_dict, benign_dict, train_ratio):
-    """
-        Generates train and test samples. Since every file sample has a number of (45, by default) walks, 
-        the walks of a single file should be saved all together for testing. Therefore, the test data is returned as dictionaries.
-        Each dict key represents a single file. The value of it is the list of walks. 
-        :param malware_dict: malware_dict created by soemd class.
-        :param benign_dict: benign_dict created by soemd class.
-        :param train_ratio: percentage of train data (0.9, 0.8 etc)
-    """
-    hash_codes = list(malware_dict.keys()) + list(benign_dict.keys())
-    X_train = []
-    y_train = []
-    X_test = dict()
-    y_test = []
+        malware_train_size = int(len(self.dataset.malware_files)*self.train_size)
+        benign_train_size = int(len(self.dataset.benign_files)*self.train_size)
 
-    random.shuffle(hash_codes) # shuffle
-    train_size = int(train_ratio*len(hash_codes))
 
-    # train data
-    for hash_code in hash_codes[:train_size]:
-        # if malware
-        if hash_code in malware_dict.keys():
-            for walk in malware_dict[hash_code]:
-                X_train.append(" ".join(walk))
-                y_train.append(1)
-        # if benign
-        else:
-            for walk in benign_dict[hash_code]:
-                X_train.append(" ".join(walk))
-                y_train.append(0)
+        #Training
+        # generate random walks for malwares
+        malware_x_train, malware_y_train = self.randomWalkGeneration_helper_train(self.dataset.malware_files[:malware_train_size], 1)
+        # generate random walks for benigns
+        benign_x_train, benign_y_train = self.randomWalkGeneration_helper_train(self.dataset.benign_files[:benign_train_size], 0)
 
-    #test data
-    for hash_code in hash_codes[:train_size]:
-        X_test[hash_code] = []
-        # if malware
-        if hash_code in malware_dict.keys():
-            for walk in malware_dict[hash_code]:
-                X_test[hash_code].append(" ".join(walk))
-            y_test.append(1)
-        else:
-            for walk in benign_dict[hash_code]:
-                X_test[hash_code].append(" ".join(walk))
-            y_test.append(0)
-    return np.array(X_train), np.array(y_train), X_test, y_test
-def test_model (model, X_test, y_test, w2v_model, walks_per_file = 45):
+        self.X_train = malware_x_train + benign_x_train
+        self.y_train = malware_y_train + benign_y_train
+
+        # Testing
+        # generate random walks for malwares
+        malware_x_test, malware_y_test = self.randomWalkGeneration_helper_test(self.dataset.malware_files[malware_train_size:], 1)
+        # generate random walks for benigns
+        benign_x_test, benign_y_test = self.randomWalkGeneration_helper_test(self.dataset.benign_files[benign_train_size:], 0)
+
+        self.X_test = malware_x_test.update(benign_x_test)
+        self.y_test = malware_y_test + benign_y_test
+
+
+def test_model (model, X_test, y_test, w2v_model):
     """
         Function to test a trained model
     """
     X_test_vectors = []
+    seq_counts = []
     for key in X_test.keys():
+        seq_counts.append(len(X_test[key]))
         for walk in X_test[key]:
             X_test_vectors.append(w2v_model.getSampleVectors(walk))
     X_test_vectors = np.array(X_test_vectors)
@@ -364,15 +361,14 @@ def test_model (model, X_test, y_test, w2v_model, walks_per_file = 45):
     FN = 0
     FP = 0
 
-    number_of_files = len(y_test)
 
-    for i in range(number_of_files):
+    for i in range(seq_counts):
         malware_decisions = 0
-        for j in range(walks_per_file):
-            walk_index = i * walks_per_file + j
+        for j in range(seq_counts[i]):
+            walk_index = i * seq_counts[i] + j
             if (y_pred[walk_index] >= 0.5):
                 malware_decisions += 1
-        if malware_decisions > 22:
+        if malware_decisions >= seq_counts[i]/2:
             prediction = 1
         else:
             prediction = 0
@@ -394,12 +390,12 @@ def test_model (model, X_test, y_test, w2v_model, walks_per_file = 45):
 if __name__ == "__main__":
     malware_datasets = ["dataset_directory"]                        ]
     benign_datasets = ["dataset_directory" ]
-    soemd_object = soemd(malware_datasets, benign_datasets, create_nodes = True )
+    soemd_object = soemd(malware_datasets, benign_datasets, create_nodes = True)
     soemd_object.generateRandomWalks()
-    X_train, y_train, X_test, y_test = train_test_split(soemd_object.malware_randomWalks_dict, soemd_object.benign_randomWalks_dict, 0.9)
-    w2v_model = w2v.w2v(sentences=np.array(X_train), vector_size=4, create_model = True)
+    X_train, y_train, X_test, y_test = soemd_object.X_train, soemd_object.y_train, soemd_object.X_test, soemd_object.y_test #train_test_split(soemd_object.malware_randomWalks_dict, soemd_object.benign_randomWalks_dict, 0.50)
+    w2v_model = w2v.w2v(sentences=X_train, create_model = True)
     X_train = w2v_model.getDatasetVectors()
-    model = cnn_model() # assuming we have a model here
-    model.fit(X_train, y_train, ...) # assuming required parameters are given
-    acc, tpr = test_model (model, X_test, y_test, w2v_model) 
+    model = cnn_model() # assuming we have a cnn model
+    model.fit(X_train, y_train, epochs = 5, batch_size=25)
+    acc, tpr = test_model (model, X_test, y_test, w2v_model)
     print("Accuracy:", acc, "and TPR:", tpr)
